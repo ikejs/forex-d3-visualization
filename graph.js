7d1c15c3877e1394216950f2012c5cb533f23a05
Ike <ikescode@gmail.com>
2021-03-03 13:27:13 -0600

    use test data

--- graph.js
+++ graph.js
@@ -13,10 +13,11 @@ const svg = d3.select("#my_dataviz")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-d3.json("https://www.freeforexapi.com/api/live?pairs=EURUSD,EURGBP,GBPUSD,USDJPY,AUDUSD,USDCHF,NZDUSD,USDCAD,USDZAR", ({ rates }) => {
+// https://www.freeforexapi.com/api/live?pairs=EURUSD,EURGBP,GBPUSD,USDJPY,AUDUSD,USDCHF,NZDUSD,USDCAD,USDZAR
+d3.json("test.json", ({ rates }) => {
 
   // console.log(rates)
-  const myRates = Object.keys(rates).map(d => ({ name: d, rate: rates[d].rate})).sort((a, b) => a.rate - b.rate).reverse();
+  const myRates = Object.keys(rates).map(d => ({ name: d, rate: rates[d].rate})).sort((a, b) => b.rate - a.rate);
   // console.log(myRates)
 
   // X axis
